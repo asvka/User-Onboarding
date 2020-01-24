0d271dfb0ac21eb0249d283a0e69ed769e664201
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-01-23 20:19:49 -0800

    MVP - Users now render properly, and styling as added

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,38 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-function userForm ( { values, errors, touched }) {
+// const NewUser = (props) => {
+//     const [user, setUser] = useState([]);
+//     useEffect(() => {
+//         status && setUser(user => [...user, status])
+//     }, [status])
+// }
+// const Users = useState([]);
+//     useEffect(() => {
+//     status && setUsers(users => [...users, status])
+//     }, [status]);
+
+const UserForm = ({ status, values, errors, touched }) => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        status && setUsers(users => [...users, status]);
+    }, [status]);
+
 // Form
     return (
         <div className="userForm">
+            <i class="far fa-heart"></i>
             <Form>
-                    <Field type="text" name="name" placeholder="Name " />
+                    <Field type="text" name="name" placeholder="Name " value={values.name} />
                     <div>
                         {touched.email && errors.email && <p>{errors.email}</p>}
-                    <Field type="text" name="email" placeholder="Email " />
+                    <Field type="text" name="email" placeholder="Email " values={values.email} />
                     </div>
                     <div>
                         {touched.password && errors.password && <p>{errors.password}</p>}
-                    <Field type="password" name="password" placeholder="Password " />
+                    <Field type="password" name="password" placeholder="Password " value={values.password} />
                     </div>
                     <label>Do you accept our Terms of Service?
                     <div>
-                    {touched.tos && errors.tos && <p>{errors.tos}</p>}
-                    <Field type="checkbox" name="tos" placeholder="Accept Terms of Service?" checked={values.tos} />
+                        {touched.tos && errors.tos && <p>{errors.tos}</p>}
+                    <Field type="checkbox" name="tos" placeholder="Accept Terms of Service?" checked={values.tos} value={values.tos} />
                     </div>
                     </label>
-                <button>
+                <button type='submit'>
                     Submit!
                 </button>
-
             </Form>
+
+            {users.map(users => (
+                <div>
+                <div className="active-users">Active Users</div>
+                <ul>
+                    <li>Name: {users.name}</li>
+                    <li>Email: {users.email}</li>
+                </ul>
+                </div>
+            ))}
+
         </div>
     )
     };
 
+
+
 const FormikUserForm = withFormik({
+
 //Mapping props to Values
     mapPropsToValues({name, email, password, tos}){
         return {
@@ -42,9 +74,12 @@ const FormikUserForm = withFormik({
             tos: tos || false,
         };
     },
+
+
 //Validation Schema
     validationSchema: Yup.object().shape({
         name: Yup.string()
+        .min(1, 'Please enter your name.')
         .required('Please enter your name.'),
 
         email: Yup.string()
@@ -58,9 +93,26 @@ const FormikUserForm = withFormik({
         tos: Yup.bool()
         .required('Please check this box to agree to our Terms of Service.')
     }),
+
 //Handle Submit
-    handleSubmit(values){
+    handleSubmit(values, { resetForm, setErrors, setSubmitting, setStatus }){
+        if (values.email === 'amhwaa@gmail.com'){
+            setErrors({ email: "Nice try, kid." })
+        } else {
+            axios
+            .post('https://reqres.in/api/users', values)
+            .then(res => {
+                console.log(res.data);
+                resetForm();
+                setSubmitting(false);
+                setStatus(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+                setSubmitting(false);
+            });
+        }
         console.log(values);
     }
-})(userForm);
+})(UserForm);
 export default FormikUserForm;
